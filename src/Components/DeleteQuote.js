fac1ceab13e1819b7b731fb1ba7eb0d10610d130
Tayyib Esau <esamu161@gmail.com>
2021-01-26 16:50:30 +1300

    Update DeleteQuote.js

--- src/Components/DeleteQuote.js
+++ src/Components/DeleteQuote.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
-import { createQuote } from "../API/API";
+//import { createQuote } from "../API/API";
 import placeholder from "../imgs/backdrop.jpg";
 
 const useStyles = makeStyles((theme) => ({
